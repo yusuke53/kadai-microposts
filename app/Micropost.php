2538a06728fed22490fce6457403026c4bb50232
yusuke.yamaki <yusuke.yamaki@rakuten.com>
2018-06-12 05:19:28 +0000

    Procfile

--- app/Micropost.php
+++ app/Micropost.php
@@ -21,4 +21,41 @@ class Micropost extends Model
     {
         return $this->belongsToMany(Micropost::class, 'micropost_favorite', 'favorited_id', 'favorite_id')->withTimestamps();
     }
+    
+    public function favorite($micropostId)
+{
+    $exist = $this->is_favoriting($micropostId);
+    $its_me = $this->id == $micropostId;
+
+    if ($exist || $its_me) {
+        return false;
+    } else {
+        // follow if not following
+        $this->favoritings()->attach($micropostId);
+        return true;
+    }
+}
+
+public function unfollow($userId)
+{
+    // confirming if already following
+    $exist = $this->is_following($userId);
+    // confirming that it is not you
+    $its_me = $this->id == $userId;
+
+
+    if ($exist && !$its_me) {
+        // stop following if following
+        $this->followings()->detach($userId);
+        return true;
+    } else {
+        // do nothing if not following
+        return false;
+    }
+}
+
+
+public function is_following($userId) {
+    return $this->followings()->where('follow_id', $userId)->exists();
+}
 }
