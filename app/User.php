035ef82ab7c736ff604d0c705b345ae82d445437
yusuke.yamaki <yusuke.yamaki@rakuten.com>
2018-06-12 04:29:47 +0000

    timeline

--- app/User.php
+++ app/User.php
@@ -81,4 +81,10 @@ public function unfollow($userId)
 public function is_following($userId) {
     return $this->followings()->where('follow_id', $userId)->exists();
 }
+ public function feed_microposts()
+    {
+        $follow_user_ids = $this->followings()-> pluck('users.id')->toArray();
+        $follow_user_ids[] = $this->id;
+        return Micropost::whereIn('user_id', $follow_user_ids);
+    }
 }
